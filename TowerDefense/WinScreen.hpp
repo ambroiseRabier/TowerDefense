a3c4f085792349f7758107d55d738b096abf5dfa
Rafired <alexis.alzoubaidy@free.fr>
2018-01-25 16:31:22 +0100

    [FIXED] WinScreen, [ADDED] Text on it

--- TowerDefense/WinScreen.hpp
+++ TowerDefense/WinScreen.hpp
@@ -12,10 +12,14 @@ namespace TowerDefense
 		public:
 			static void init();
 			static void open();
+			static void destroy();
+			static void setScore(int pScore);
 			static void close();
 		private:
 			static std::unique_ptr<BaseButton> menu_return_btn;
 			static void on_click_menu_return();
+			static void createText();
+			static float score;
 		};
 	}
 }
