e936a52b45b06fb8cba45319544c76b14fa70fdf
ambroiseRabier <rabier.ambroise@outlook.fr>
2018-02-07 12:42:08 +0100

    [Updated] LevelDesign.

--- TowerDefense/LevelDesign.hpp
+++ TowerDefense/LevelDesign.hpp
@@ -13,7 +13,7 @@ namespace TowerDefense
 
 			const MapParams level0 = {
 				// name displayed
-				"Last Level !",
+				"Hello world",
 				// preparation time
 				3,
 				// start money
@@ -63,8 +63,78 @@ namespace TowerDefense
 				}
 			};
 
-			const std::array<MapParams,1> map_array = {
-				level0
+			const MapParams level1 = {
+				// name displayed
+				"Last Level !",
+				// preparation time
+				3,
+				// start money
+				150.f,
+				// tiles
+				{
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+					{{Moutain_Deco}, {Grass_Build}, {Grass_Build}, {Grass_Build}, {Grass_Build}, {Grass_Build},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Road_Walk}, {Road_Walk}, {Road_Walk}, {Grass_Build},},
+					{{Grass_Build}, {Spawn_Other}, {Road_Walk}, {Grass_Build}, {Road_Walk}, {Road_Walk}, {Road_Walk}, {Road_Walk}, {Road_Walk}, {Castle_Other},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Road_Walk}, {Road_Walk}, {Road_Walk}, {Grass_Build},},
+					{{Moutain_Deco}, {Grass_Build}, {Moutain_Deco}, {Grass_Build}, {Grass_Build}, {Grass_Build},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+					{{Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco}, {Moutain_Deco},},
+				},
+				// waveParams vector
+				{
+					// waveParam
+					{
+						0.0f, // additionnal time before next wave
+						{
+							{1.f, Heal}, // time before next minon and current minion spawned.
+							{2.f, Peon},
+							{2.f, Peon},
+							{2.f, Peon},
+							{2.f, Tank},
+						},
+					},					
+					{
+						2.0f,
+						{
+							{1.f, Peon},
+							{1.f, Peon},
+							{1.f, Peon},
+							{1.f, Heal},
+							{0.f, Peon},
+							{0.f, Peon},
+							{0.f, Peon},
+							{0.f, Peon},
+							{1.f, Peon},
+							{0, Peon},
+						},
+					},
+					{
+						3.0f,
+						{
+							{1.f, Tank},
+							{1.f, Tank},
+							{1.f, Tank},
+							{0.f, Peon},
+							{0.f, Peon},
+							{0.f, Peon},
+							{0.f, Peon},
+							{1.f, Peon},
+							{1.f, Peon},
+							{1.f, Peon},
+							{1.f, Peon},
+							{1.f, Peon},
+						},
+					},
+				}
+			};
+
+			const std::array<MapParams,2> map_array = {
+				level0,
+				level1
 			};
 
 		}
